353dbf868778cc13f41194e4ed8c35c271d7ef60
Sainigam14 <116962883+Sainigam14@users.noreply.github.com>
2023-06-08 20:29:19 +0000

    login authentication

--- database.py
+++ database.py
@@ -1,10 +1,57 @@
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, Column, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql+pymysql://m3wmcgloi0pstrejrhsm:pscale_pw_UnURrEs7VX3P16klv9GkWAjvyxroDWsEMhAyPzxL0Fg@aws.connect.psdb.cloud/finflex?charset=utf8mb4", connect_args={
+import os
+my_secret = os.environ['DB_details']
+
+engine = create_engine(my_secret, connect_args={
         "ssl": {
             "ssl_ca": "/etc/ssl/cert.pem"
         }
     })
+
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    firstname = Column(String(20))
+    lastname = Column(String(20))
+    email = Column(String(50), unique=True, primary_key=True)
+    password = Column(String(50))
+
+    def __init__(self, firstname, lastname, email, password):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.password = password
+
+def total_expense(user_email):
+    with engine.connect() as conn:
+      query = text("SELECT COALESCE(SUM(amount), 0) AS total_amount FROM expense WHERE email = :user_email")
+      result = conn.execute(query, {"user_email" : user_email}).fetchone()
+      total_amount = result[0]
+      return int(total_amount)
+
+def total_income(user_email):
+  with engine.connect() as conn:
+    query = text("SELECT COALESCE(SUM(amount), 0) AS total_amount FROM income WHERE email = :user_email")
+    result = conn.execute(query, {"user_email" : user_email}).fetchone()
+    total_amount = result[0]
+    return int(total_amount)
+
+def store_expense(user_email, category, amount, date_time, description):
+  with engine.connect() as conn:
+    query = text("INSERT INTO expense (email, category, amount, date_time, description) VALUES (:user_email, :category, :amount, :date_time, :description)")
+    conn.execute(query, {"user_email" : user_email, "category": category, "amount": amount, "date_time": date_time, "description": description})
+
+def store_income(user_email, category, amount, date_time, description):
+  with engine.connect() as conn:
+    query = text("INSERT INTO income (email, category, amount, date_time, description) VALUES (:user_email, :category, :amount, :date_time, :description)")
+    conn.execute(query, {"user_email" : user_email, "category": category, "amount": amount, "date_time": date_time, "description": description})
+
 
 def get_expense():
   with engine.connect() as conn:
@@ -13,27 +60,3 @@ def get_expense():
     for row in result.all():
       expenses.append(row._asdict())
     return expenses
-
-def total_expense():
-    with engine.connect() as conn:
-      query = text("SELECT COALESCE(SUM(amount), 0) AS total_amount FROM expense")
-      result = conn.execute(query).fetchone()
-      total_amount = result[0]
-      return int(total_amount)
-
-def total_income():
-  with engine.connect() as conn:
-    result = conn.execute(text("SELECT SUM(amount) AS total_amount FROM income"))
-    total_amount = result.all()
-    value = int(total_amount[0]._asdict()['total_amount'])
-    return value
-
-def store_expense(category, amount, date_time, description):
-  with engine.connect() as conn:
-    query = text("INSERT INTO expense (category, amount, date_time, description) VALUES (:category, :amount, :date_time, :description)")
-    conn.execute(query, {"category": category, "amount": amount, "date_time": date_time, "description": description})
-
-def store_income(category, amount, date_time, description):
-  with engine.connect() as conn:
-    query = text("INSERT INTO income (category, amount, date_time, description) VALUES (:category, :amount, :date_time, :description)")
-    conn.execute(query, {"category": category, "amount": amount, "date_time": date_time, "description": description})
--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-from flask import Flask, render_template, jsonify, request, redirect
-from database import total_expense, get_expense, total_income, store_expense,store_income
-from markupsafe import Markup
-import secrets
+from flask import Flask, render_template, jsonify, request, redirect, session
+from database import engine, Session, User, total_expense, get_expense, total_income, store_expense,store_income
 
 app = Flask('__name__')
+app.secret_key = 'my_secret_key'
 
 @app.route('/')
 def FinFlex():
   return render_template('index.html')
-
-@app.route('/home')
-def home():
-  return render_template('home.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
 @app.route('/signup')
 def sign():
@@ -24,38 +15,111 @@ def sign():
 @app.template_filter('abs')
 def filter_abs(value):
     return abs(value)
+  
+# Protected route for the dashboard
+@app.route('/home')
+def home():
+  if 'user_email' in session:
+        user_email = session['user_email']
 
+        db_session = Session()
+        user = db_session.get(User, user_email)
+        db_session.close()
+
+        return render_template('home.html', user=user)
+
+    # User is not logged in, redirect to login page
+  return redirect('/login')
+  
 @app.route('/home/personalfinance', methods=['GET', 'POST'])
 def personalfinance():
-  if request.method == 'POST':
-    form_name = request.form.get('form_name')
-    print(form_name)
-    if form_name == 'expense':
-      category = request.form.get('categoryInput')
-      amount = int(request.form.get('amountInput'))
-      date_time = request.form.get('dateTimeInput')
-      description = request.form.get('descriptionInput')
-      store_expense(category, amount, date_time, description)
-      return redirect(request.url)
-    if form_name == 'income':
-      category = request.form.get('categoryInput')
-      amount = int(request.form.get('amountInput'))
-      date_time = request.form.get('dateTimeInput')
-      description = request.form.get('descriptionInput')
-      print(category)
-      print(amount)
-      print(date_time)
-      print(description)
-      store_income(category, amount, date_time, description)
-      return redirect(request.url)
-  expense = total_expense()
-  income = total_income()
-  balance = income - expense
-  return render_template('personalfinance.html',
-                         total_expense=expense,
-                         total_income=income,
-                         balance=balance)
+  if 'user_email' in session:
+    user_email = session['user_email']
+    if request.method == 'POST':
+      form_name = request.form.get('form_name')
+      if form_name == 'expense':
+        category = request.form.get('categoryInput')
+        amount = int(request.form.get('amountInput'))
+        date_time = request.form.get('dateTimeInput')
+        description = request.form.get('descriptionInput')
+        store_expense(user_email, category, amount, date_time, description)
+        return redirect('/home/personalfinance?message=Successfully added expense details')
+      if form_name == 'income':
+        category = request.form.get('categoryInput')
+        amount = int(request.form.get('amountInput'))
+        date_time = request.form.get('dateTimeInput')
+        description = request.form.get('descriptionInput')
+        store_income(user_email, category, amount, date_time, description)
+        return redirect('/home/personalfinance?message=Successfully added income details')
+    expense = total_expense(user_email)
+    income = total_income(user_email)
+    balance = income - expense
+    print(balance)
+    return render_template('personalfinance.html',
+                           total_expense=expense,
+                           total_income=income,
+                           balance=balance)
+  return redirect('/login')
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        db_session = Session()
+        user = db_session.query(User).filter_by(email=email).first()
+        db_session.close()
+
+        if user and user.password == password:
+            session['user_email'] = user.email
+            return redirect('/home')
+        elif not user:
+            error_message = 'Please create an account'
+            return render_template('login.html', error_message=error_message)
+        else:
+            error_message = 'Invalid password'
+            return render_template('login.html', error_message=error_message)
+          
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        db_session = Session()
+        existing_user = db_session.query(User).filter_by(email=email).first()
+
+        if existing_user:
+            error_message = 'User already exists. Please log in.'
+            db_session.close()
+            return render_template('signup.html', error_message=error_message)
+
+        new_user = User(firstname=firstname, lastname=lastname, email=email, password=password)
+
+        db_session.add(new_user)
+        db_session.commit()
+        db_session.close()
+
+        session['user_email'] = email
+
+        # Redirect to the login page after successful signup
+        return redirect('/home')
+
+    # Render the signup form for GET requests
+    return render_template('signup.html')
+
+
+
+@app.route('/home/personalfinance/signout')
+def signout():
+  session.clear();
+  return redirect('/')
 
 
 
