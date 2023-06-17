1e12bea39f8b4bced295e3037ff34344ff94444d
Sainigam14 <116962883+Sainigam14@users.noreply.github.com>
2023-06-17 15:09:13 +0000

    months seperated

--- database.py
+++ database.py
@@ -4,71 +4,115 @@ from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
 import os
+
 my_secret = os.environ['DB_details']
 
-engine = create_engine(my_secret, connect_args={
-        "ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem"
-        }
-    })
+engine = create_engine(my_secret,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = 'users'
+  __tablename__ = 'users'
 
-    firstname = Column(String(20))
-    lastname = Column(String(20))
-    email = Column(String(50), unique=True, primary_key=True)
-    password = Column(String(50))
+  firstname = Column(String(20))
+  lastname = Column(String(20))
+  email = Column(String(50), unique=True, primary_key=True)
+  password = Column(String(50))
 
-    def __init__(self, firstname, lastname, email, password):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.email = email
-        self.password = password
+  def __init__(self, firstname, lastname, email, password):
+    self.firstname = firstname
+    self.lastname = lastname
+    self.email = email
+    self.password = password
+
 
 def store_expense(user_email, category, amount, date_time, description):
-    if not date_time:
-        # If date_time is not provided, use the current date and time
-        date_time = datetime.now()
-    else:
-        # Parse the provided date_time string into a datetime object
-        date_time = datetime.strptime(date_time, '%Y-%m-%dT%H:%M')
+  if not date_time:
+    # If date_time is not provided, use the current date and time
+    date_time = datetime.now()
+  else:
+    # Parse the provided date_time string into a datetime object
+    date_time = datetime.strptime(date_time, '%Y-%m-%dT%H:%M')
 
-    # Format the datetime object as a string in the MySQL TIMESTAMP format
-    date_time_formatted = date_time.strftime('%Y-%m-%d %H:%M:%S')
+  # Format the datetime object as a string in the MySQL TIMESTAMP format
+  date_time_formatted = date_time.strftime('%Y-%m-%d %H:%M:%S')
 
-    with engine.connect() as conn:
-      query = text("INSERT INTO expense (email, category, amount, date_time, description) VALUES (:user_email, :category, :amount, :date_time, :description)")
-      conn.execute(query, {"user_email": user_email, "category": category, "amount": amount, "date_time": date_time_formatted, "description": description})
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO expense (email, category, amount, date_time, description) VALUES (:user_email, :category, :amount, :date_time, :description)"
+    )
+    conn.execute(
+      query, {
+        "user_email": user_email,
+        "category": category,
+        "amount": amount,
+        "date_time": date_time_formatted,
+        "description": description
+      })
 
 
 def store_income(user_email, category, amount, date_time, description):
+  if not date_time:
+    # If date_time is not provided, use the current date and time
+    date_time = datetime.now()
+  else:
+    # Parse the provided date_time string into a datetime object
+    date_time = datetime.strptime(date_time, '%Y-%m-%dT%H:%M')
+
+  # Format the datetime object as a string in the MySQL TIMESTAMP format
+  date_time_formatted = date_time.strftime('%Y-%m-%d %H:%M:%S')
+  
   with engine.connect() as conn:
-    query = text("INSERT INTO income (email, category, amount, date_time, description) VALUES (:user_email, :category, :amount, :date_time, :description)")
-    conn.execute(query, {"user_email" : user_email, "category": category, "amount": amount, "date_time": date_time, "description": description})
+    query = text(
+      "INSERT INTO income (email, category, amount, date_time, description) VALUES (:user_email, :category, :amount, :date_time, :description)"
+    )
+    conn.execute(
+      query, {
+        "user_email": user_email,
+        "category": category,
+        "amount": amount,
+        "date_time": date_time,
+        "description": description
+      })
+
 
 def get_total_expense_and_income(month_id, year, user_email):
-    with engine.connect() as conn:
-      
-        # Calculate total expense for the specified month
-        expense_query = text("SELECT COALESCE(SUM(amount), 0) AS total_expense FROM expense WHERE YEAR(date_time) = :year AND MONTH(date_time) = :month AND email = :user_email")
-        expense_result = conn.execute(expense_query, {"year": year, "month": month_id, "user_email": user_email})
-        total_expense = expense_result.fetchone()[0]
-      
-        # Calculate total income for the specified month
-        income_query = text("SELECT COALESCE(SUM(amount), 0) AS total_income FROM income WHERE YEAR(date_time) = :year AND MONTH(date_time) = :month AND email = :user_email")
-        income_result = conn.execute(income_query, {"year": year, "month": month_id, "user_email": user_email})
-        total_income = income_result.fetchone()[0]
-
-        return total_expense, total_income
-
-def get_expense():
   with engine.connect() as conn:
-    result = conn.execute(text("select * from expense"))
-    expenses = []
+
+    # Calculate total expense for the specified month
+    expense_query = text(
+      "SELECT COALESCE(SUM(amount), 0) AS total_expense FROM expense WHERE YEAR(date_time) = :year AND MONTH(date_time) = :month AND email = :user_email"
+    )
+    expense_result = conn.execute(expense_query, {
+      "year": year,
+      "month": month_id,
+      "user_email": user_email
+    })
+    total_expense = expense_result.fetchone()[0]
+
+    # Calculate total income for the specified month
+    income_query = text(
+      "SELECT COALESCE(SUM(amount), 0) AS total_income FROM income WHERE YEAR(date_time) = :year AND MONTH(date_time) = :month AND email = :user_email"
+    )
+    income_result = conn.execute(income_query, {
+      "year": year,
+      "month": month_id,
+      "user_email": user_email
+    })
+    total_income = income_result.fetchone()[0]
+
+    return total_expense, total_income
+
+
+def get_transactions():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from expense where email = :user_email"))
+    transactions = []
     for row in result.all():
       expenses.append(row._asdict())
     return expenses
--- main.py
+++ main.py
@@ -1,40 +1,94 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-from database import Session, User, store_expense, store_income, datetime, get_total_expense_and_income
+from database import Session, User, store_expense, store_income, datetime, get_total_expense_and_income, get_transactions
 
 app = Flask('__name__')
 app.secret_key = 'my_secret_key'
+
 
 @app.route('/')
 def FinFlex():
   return render_template('index.html')
 
-@app.route('/home/blog')
-def blog():
-  if 'user_email' in session:
-        return render_template('blog.html')
-    # User is not logged in, redirect to login page
-  return redirect('/login')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    db_session = Session()
+    user = db_session.query(User).filter_by(email=email).first()
+    db_session.close()
+
+    if user and user.password == password:
+      session['user_email'] = user.email
+      month_id = '0' + str((datetime.now().month))
+      return redirect(url_for('home', month_id=month_id))
+    elif not user:
+      error_message = 'Please create an account'
+      return render_template('login.html', error_message=error_message)
+    else:
+      error_message = 'Invalid password'
+      return render_template('login.html', error_message=error_message)
+
+  return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+  if request.method == 'POST':
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    db_session = Session()
+    existing_user = db_session.query(User).filter_by(email=email).first()
+
+    if existing_user:
+      error_message = 'User already exists. Please log in.'
+      db_session.close()
+      return render_template('signup.html', error_message=error_message)
+
+    new_user = User(firstname=firstname,
+                    lastname=lastname,
+                    email=email,
+                    password=password)
+
+    db_session.add(new_user)
+    db_session.commit()
+    db_session.close()
+
+    session['user_email'] = email
+
+    # Redirect to the home page after successful signup
+    month_id = '0' + str((datetime.now().month))
+    return redirect(url_for('home', month_id=month_id))
+
+  # Render the signup form for GET requests
+  return render_template('signup.html')
+
 
 @app.template_filter('abs')
 def filter_abs(value):
-    return abs(value)
-  
+  return abs(value)
+
+
 # Protected route for the dashboard
-@app.route('/home')
-def home():
+@app.route('/home/<month_id>')
+def home(month_id):
   if 'user_email' in session:
-        user_email = session['user_email']
-        month_id = int((datetime.now().month))
+    user_email = session['user_email']
 
-        db_session = Session()
-        user = db_session.get(User, user_email)
-        db_session.close()
+    db_session = Session()
+    user = db_session.get(User, user_email)
+    db_session.close()
 
-        return render_template('home.html', user=user, month_id=month_id)
+    return render_template('home.html', user=user, month_id=month_id)
 
-    # User is not logged in, redirect to login page
+  # User is not logged in, redirect to login page
   return redirect('/login')
-  
+
+
 @app.route('/home/personalfinance/<month_id>', methods=['GET', 'POST'])
 def personalfinance(month_id):
   year = datetime.now().year
@@ -62,72 +116,32 @@ def personalfinance(month_id):
     print(expense, income)
     balance = income - expense
     return render_template('personalfinance.html',
-                            total_expense=expense,
-                            total_income=income,
-                            balance=balance,
-                            month_id=month_id)
+                           total_expense=expense,
+                           total_income=income,
+                           balance=balance,
+                           month_id=month_id)
   return redirect('/login')
 
+@app.route('/home/transactions/<month_id>')
+def transactions(month_id):
+  if 'user_email' in session:
+    user_email = session['user_email']
+    transactions = get_transactions(month_id, user_email)
+    return render_template('transactions.html', month_id=month_id)
+  # User is not logged in, redirect to login page
+  return redirect('/login')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        db_session = Session()
-        user = db_session.query(User).filter_by(email=email).first()
-        db_session.close()
-
-        if user and user.password == password:
-            session['user_email'] = user.email
-            return redirect('/home')
-        elif not user:
-            error_message = 'Please create an account'
-            return render_template('login.html', error_message=error_message)
-        else:
-            error_message = 'Invalid password'
-            return render_template('login.html', error_message=error_message)
-          
-    return render_template('login.html')
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        firstname = request.form.get('firstname')
-        lastname = request.form.get('lastname')
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        db_session = Session()
-        existing_user = db_session.query(User).filter_by(email=email).first()
-
-        if existing_user:
-            error_message = 'User already exists. Please log in.'
-            db_session.close()
-            return render_template('signup.html', error_message=error_message)
-
-        new_user = User(firstname=firstname, lastname=lastname, email=email, password=password)
-
-        db_session.add(new_user)
-        db_session.commit()
-        db_session.close()
-
-        session['user_email'] = email
-
-        # Redirect to the login page after successful signup
-        return redirect('/home')
-
-    # Render the signup form for GET requests
-    return render_template('signup.html')
-
-
+@app.route('/home/blog')
+def blog():
+  if 'user_email' in session:
+    return render_template('blog.html')
+  # User is not logged in, redirect to login page
+  return redirect('/login')
 
 @app.route('/home/personalfinance/signout')
 def signout():
-  session.clear();
+  session.clear()
   return redirect('/')
-
 
 
 app.run(host='0.0.0.0', port=8080)
